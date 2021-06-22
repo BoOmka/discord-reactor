e85cbab2a70eac913921ba9eb14e2965e6f78f05
azhilin <zhilin.alx@gmail.com>
2021-06-22 16:21:20 +0300

    Add some tests

---
+++ tests/test_helpers.py
@@ -0,0 +1,35 @@
+import pytest
+
+from helpers import get_message_id_from_id_or_url
+
+
+class TestGetMessageIdFromIdOrUrl:
+    @pytest.mark.parametrize(
+        "message_id_or_url, expected_message_id",
+        (
+            ("100500", 100500),
+            ("https://discord.com/channels/644645112692867073/665817309478125583/856879535811067954", 856879535811067954),
+            ("1", 1),
+        )
+    )
+    def test_happy_path(self, message_id_or_url, expected_message_id):
+        # act
+        result = get_message_id_from_id_or_url(message_id_or_url)
+
+        # assert
+        assert result == expected_message_id
+
+    @pytest.mark.parametrize(
+        "message_id_or_url",
+        (
+            "foo",
+            "https://discord.com/channels/644645112692867073/665817309478125583/856879535811067954/",
+            "https://discord.com/channels/644645112692867073/665817309478125583/856879535dasdasd811067954",
+            "123123sadasd15213",
+            "",
+        )
+    )
+    def test_contains_nondigits__raises_value_error(self, message_id_or_url):
+        # act & assert
+        with pytest.raises(ValueError):
+            get_message_id_from_id_or_url(message_id_or_url)
